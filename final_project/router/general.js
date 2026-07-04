c7d1c138fcf9de65204bbc643cdc266c66014fbc
anaDevlol <homara6@gmail.com>
2026-07-04 19:51:57 -0400

    do11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,15 +47,29 @@ public_users.get('/', async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    const book = books[isbn];
-    if (book) {
-        return res.status(200).json(book);
-    } else {
-        return res.status(404).json({ message: "Book not found" });
-    }
- });
+public_users.get('/isbn/:isbn', async function (req, res) {
+
+  const isbn = req.params.isbn;
+
+  try {
+
+    const getBookByISBN = new Promise((resolve, reject) => {
+      if (books[isbn]) {
+        resolve(books[isbn]);
+      } else {
+        reject("Book not found");
+      }
+    });
+
+    const data = await getBookByISBN;
+
+    return res.status(200).json(data);
+
+  } catch (error) {
+    return res.status(404).json({ message: error });
+  }
+
+});
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
